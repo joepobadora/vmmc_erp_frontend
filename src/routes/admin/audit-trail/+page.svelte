e9fd6f8bf8e9f2a5937ea4ee9975e45e3925048f
joepobadora <joelouis.pobadora@gmail.com>
2026-04-17 07:31:38 +0800

    converted audit trail ui to use component helper

--- src/routes/admin/audit-trail/+page.svelte
+++ src/routes/admin/audit-trail/+page.svelte
@@ -4,7 +4,7 @@
     import App from '$lib/assets/js/bootstrap';
     import { Alert } from '$lib/stores/alert';
     import { goto } from '$app/navigation';
-    import { ta } from 'zod/v4/locales';
+    import j from '$lib/components/helper';
 
     let loadingData = $state('false');
     let logs = $state([]);
@@ -72,123 +72,112 @@
     }
 </script>
 
-<div class="row">
-    <div class="col">
-        <!-- controls -->
-        <div class="row mb-4">
-            <!-- breadcrumbs -->
+<j.RowCol>
+    <nav style="--bs-breadcrumb-divider: '>';">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item small"><a href="/admin">Admin Console</a></li>
+            <li class="breadcrumb-item small active">Audit Trail</li>
+        </ol>
+    </nav>
+</j.RowCol>
+
+<j.Row endy>
+    <!-- date from -->
+    <j.Col auto>
+        <label for="docmngtMyDocumentsFromDatePicker" class="small text-muted ms-1">Date from</label>
+        <input bind:value={filter.start_date} type="date" class="form-control form-control-sm" id="docmngtMyDocumentsFromDatePicker" placeholder="YYYY-MM-DD" />
+    </j.Col>
+
+    <!-- date to -->
+    <j.Col auto>
+        <label for="docmngtMyDocumentsToDatePicker" class="small text-muted ms-1">Date to</label>
+        <input bind:value={filter.end_date} type="date" class="form-control form-control-sm" id="docmngtMyDocumentsToDatePicker" placeholder="YYYY-MM-DD" />
+    </j.Col>
+
+    <!-- search -->
+    <j.Col>
+        <j.Row mb="0">
+            <j.Col>
+                <label for="docmngtMyDocumentsSearchInput" class="small text-muted ms-1">Search</label>
+            </j.Col>
+            <j.Col auto>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="checkDefault" />
+                    <label class="form-check-label small text-muted" for="checkDefault">Advanced search</label>
+                    <i title="This will include changed database 'values' in your query." class="bi bi-question-circle-fill ms-1 text-primary"></i>
+                </div>
+            </j.Col>
+        </j.Row>
+        <input bind:value={filter.search} type="text" class="form-control form-control-sm" placeholder="Search by name..." id="docmngtMyDocumentsSearchInput" />
+    </j.Col>
+
+    <!-- reset button -->
+    <j.Col auto>
+        <button onclick={resetFilter} type="button" class="btn btn-outline-primary btn-sm px-3" id="docmngtMyDocumentsResetButton">Reset</button>
+    </j.Col>
+</j.Row>
+
+<j.Card>
+    {#if loadingData}
+        <div class="d-flex justify-content-center p-4">
+            <div class="spinner-border text-primary" role="status"></div>
+        </div>
+    {:else}
+        <div class="row px-2">
             <div class="col">
-                <nav style="--bs-breadcrumb-divider: '>';">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item small"><a href="/admin">Admin Console</a></li>
-                        <li class="breadcrumb-item small active">Audit Trail</li>
-                    </ol>
-                </nav>
+                <p class="small text-muted">Timestamp</p>
+            </div>
+            <div class="col">
+                <p class="small text-muted">Client Route</p>
+            </div>
+            <div class="col">
+                <p class="small text-muted">Action Taken</p>
+            </div>
+            <div class="col">
+                <p class="small text-muted">Entity</p>
+            </div>
+            <div class="col">
+                <p class="small text-muted">Actor</p>
             </div>
         </div>
-
-        <div class="row mb-4">
-            <!-- date from -->
-            <div class="col-auto">
-                <label for="docmngtMyDocumentsFromDatePicker" class="small text-muted ms-1">Date from</label>
-                <input bind:value={filter.start_date} type="date" class="form-control form-control-sm" id="docmngtMyDocumentsFromDatePicker" placeholder="YYYY-MM-DD" />
-            </div>
-
-            <!-- date to -->
-            <div class="col-auto">
-                <label for="docmngtMyDocumentsToDatePicker" class="small text-muted ms-1">Date to</label>
-                <input bind:value={filter.end_date} type="date" class="form-control form-control-sm" id="docmngtMyDocumentsToDatePicker" placeholder="YYYY-MM-DD" />
-            </div>
-
-            <!-- search -->
-            <div class="col">
-                <div class="row">
-                    <div class="col">
-                        <label for="docmngtMyDocumentsSearchInput" class="small text-muted ms-1">Search</label>
-                    </div>
-                    <div class="col-auto">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="checkDefault" />
-                            <label class="form-check-label small text-muted" for="checkDefault">Advanced search</label>
-                            <i title="This will include changed database 'values' in your query." class="bi bi-question-circle-fill ms-1 text-primary"></i>
-                        </div>
+        <Table data={logs} enableTotalCount enablePagination pageSize="50" bind:currentPage={tablePage}>
+            <div slot="row" let:item class="row border-bottom custom-row small">
+                <div class="col">
+                    <div>
+                        <span
+                            class="custom-link text-danger"
+                            onclick={() => {
+                                goto(page.url.pathname + `/view/${item.id}${p.toString()}`);
+                            }}>{App.Format.date(item.created_at).toDatetime()}</span
+                        >
                     </div>
                 </div>
-                <input bind:value={filter.search} type="text" class="form-control form-control-sm" placeholder="Search by name..." id="docmngtMyDocumentsSearchInput" />
-            </div>
-
-            <!-- reset button -->
-            <div class="col-auto d-flex align-items-end">
-                <button onclick={resetFilter} type="button" class="btn btn-outline-primary btn-sm px-3" id="docmngtMyDocumentsResetButton">Reset</button>
-            </div>
-        </div>
-
-        <!-- table -->
-        <div class="card border-0 shadow-sm px-3">
-            <div class="card-body">
-                {#if loadingData}
-                    <div class="d-flex justify-content-center p-4">
-                        <div class="spinner-border text-primary" role="status"></div>
+                <div class="col">
+                    <div>
+                        <span>{item.client_route}</span>
                     </div>
-                {:else}
-                    <div class="row px-2">
-                        <div class="col">
-                            <p class="small text-muted">Timestamp</p>
-                        </div>
-                        <div class="col">
-                            <p class="small text-muted">Client Route</p>
-                        </div>
-                        <div class="col">
-                            <p class="small text-muted">Action Taken</p>
-                        </div>
-                        <div class="col">
-                            <p class="small text-muted">Entity</p>
-                        </div>
-                        <div class="col">
-                            <p class="small text-muted">Actor</p>
-                        </div>
+                </div>
+                <div class="col">
+                    <div>
+                        <span
+                            >{(() => {
+                                const id = (item.api_endpoint.match(/\/(\d+)$/) || [])[1];
+                                return item.details + (id ? ': ' + id : '');
+                            })()}</span
+                        >
                     </div>
-                    <Table data={logs} enableTotalCount enablePagination pageSize="50" bind:currentPage={tablePage}>
-                        <div slot="row" let:item class="row border-bottom custom-row small">
-                            <div class="col">
-                                <div>
-                                    <span
-                                        class="custom-link text-danger"
-                                        onclick={() => {
-                                            goto(page.url.pathname + `/view/${item.id}${p.toString()}`);
-                                        }}>{App.Format.date(item.created_at).toDatetime()}</span
-                                    >
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div>
-                                    <span>{item.client_route}</span>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div>
-                                    <span
-                                        >{(() => {
-                                            const id = (item.api_endpoint.match(/\/(\d+)$/) || [])[1];
-                                            return item.details + (id ? ': ' + id : '');
-                                        })()}</span
-                                    >
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div>
-                                    <span>{item.entity}</span>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div>
-                                    <span>{item.full_name_2}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </Table>
-                {/if}
+                </div>
+                <div class="col">
+                    <div>
+                        <span>{item.entity}</span>
+                    </div>
+                </div>
+                <div class="col">
+                    <div>
+                        <span>{item.full_name_2}</span>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
+        </Table>
+    {/if}
+</j.Card>
